56ac1ffb1b17e1807154c30f4caaae8764af0257
dostal-nbk <dostal@neubaukompass.de>
2026-02-03 11:56:16 +0100

    composer für symfony und php upgrade angepasst.

--- DependencyInjection/MbDoctrineLogExtension.php
+++ DependencyInjection/MbDoctrineLogExtension.php
@@ -8,16 +8,15 @@ use Mb\DoctrineLogBundle\EventSubscriber\Logger;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class MbDoctrineLogExtension extends Extension
 {
-
     /** @inheritDoc */
-    public function load(array $configs, ContainerBuilder $containerBuilder)
+    public function load(array $configs, ContainerBuilder $containerBuilder): void
     {
-        $loader = new XmlFileLoader($containerBuilder, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
         $configuration = $this->getConfiguration($configs, $containerBuilder);
         $config = $this->processConfiguration($configuration, $configs);
